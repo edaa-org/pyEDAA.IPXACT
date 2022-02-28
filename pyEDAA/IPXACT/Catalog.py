c51ca8fa76d815bb5b76346edd9e4d93d52f7b3a
Patrick Lehmann <Paebbels@gmail.com>
2022-02-28 10:56:25 +0100

    Updated file headers.

--- pyEDAA/IPXACT/Catalog.py
+++ pyEDAA/IPXACT/Catalog.py
@@ -1,31 +1,33 @@
 # ==================================================================================================================== #
-#              ___ ______  __    _    ____ _____
-#  _ __  _   _|_ _|  _ \ \/ /   / \  / ___|_   _|
-# | '_ \| | | || || |_) \  /   / _ \| |     | |
-# | |_) | |_| || ||  __//  \  / ___ \ |___  | |
-# | .__/ \__, |___|_|  /_/\_\/_/   \_\____| |_|
-# |_|    |___/
-# =============================================================================
-# Authors:            Patrick Lehmann
-#
-# Python module:      A DOM based IP-XACT implementation for Python
-#
-# License:
-# ==============================================================================
-# Copyright 2007-2016 Patrick Lehmann - Dresden, Germany
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
+#              _____ ____    _        _      ___ ______  __    _    ____ _____                                         #
+#  _ __  _   _| ____|  _ \  / \      / \    |_ _|  _ \ \/ /   / \  / ___|_   _|                                        #
+# | '_ \| | | |  _| | | | |/ _ \    / _ \    | || |_) \  /   / _ \| |     | |                                          #
+# | |_) | |_| | |___| |_| / ___ \  / ___ \ _ | ||  __//  \  / ___ \ |___  | |                                          #
+# | .__/ \__, |_____|____/_/   \_\/_/   \_(_)___|_|  /_/\_\/_/   \_\____| |_|                                          #
+# |_|    |___/                                                                                                         #
+# ==================================================================================================================== #
+# Authors:                                                                                                             #
+#   Patrick Lehmann                                                                                                    #
+#                                                                                                                      #
+# License:                                                                                                             #
+# ==================================================================================================================== #
+# Copyright 2017-2022 Patrick Lehmann - Bötzingen, Germany                                                             #
+# Copyright 2007-2016 Patrick Lehmann - Dresden, Germany                                                               #
+#                                                                                                                      #
+# Licensed under the Apache License, Version 2.0 (the "License");                                                      #
+# you may not use this file except in compliance with the License.                                                     #
+# You may obtain a copy of the License at                                                                              #
+#                                                                                                                      #
+#   http://www.apache.org/licenses/LICENSE-2.0                                                                         #
+#                                                                                                                      #
+# Unless required by applicable law or agreed to in writing, software                                                  #
+# distributed under the License is distributed on an "AS IS" BASIS,                                                    #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                                             #
+# See the License for the specific language governing permissions and                                                  #
+# limitations under the License.                                                                                       #
+#                                                                                                                      #
+# SPDX-License-Identifier: Apache-2.0                                                                                  #
+# ==================================================================================================================== #
 #
 from typing import List
 
