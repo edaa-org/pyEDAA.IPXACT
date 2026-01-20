bd36d7f03d33907d8d42f3049a047ebd3a3e74e4
Patrick Lehmann <Paebbels@gmail.com>
2026-01-20 23:40:38 +0100

    Bumped dependencies.

--- pyEDAA/IPXACT/__init__.py
+++ pyEDAA/IPXACT/__init__.py
@@ -48,7 +48,7 @@ __author__ =    "Patrick Lehmann"
 __email__ =     "Paebbels@gmail.com"
 __copyright__ = "2016-2026, Patrick Lehmann"
 __license__ =   "Apache License, Version 2.0"
-__version__ =   "0.6.4"
+__version__ =   "0.6.5"
 
 
 @export
